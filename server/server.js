db92b9f6f47a9377a0030a9c373daf7ed7cae796
skhadk10 <rickeykhd@gmail.com>
2024-12-15 21:58:31 +1100

    server setup witl mongodb

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+// create a database connection
+mongoose
+  .connect("mongodb://localhost:27017/ecommerce")
+  .then(() => console.log("Mongodb connected"))
+  .catch((error) => console.log(error));
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "cache-control",
+      "Expires",
+      "Pragma",
+    ],
+    Credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is runnin g on port ${PORT}`));
